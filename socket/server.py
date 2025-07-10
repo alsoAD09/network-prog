14e4da2b7495bd8e19c51cf5be6f9414b88fc359
alsoAD09 <edworked2005@gmail.com>
2025-07-10 16:25:56 +0530

    initial commit

---
+++ socket/server.py
@@ -0,0 +1,16 @@
+import socket
+Host='192.168.56.1'
+port=9090
+
+server =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((Host, port))
+
+server.listen(5)
+while True:
+  communication_socket, address  =server.accept()
+  print(f"Connected to{address}")
+  message=communication_socket.recv(1024).decode('utf-8')
+  print(f"message from client is:{message} ")
+  communication_socket.send("got your message,thank you".encode('utf-8'))
+  communication_socket.close()
+  print(f"connection with{address}ended")
